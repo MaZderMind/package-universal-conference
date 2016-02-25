77b088eef78fbe54c09bba71a86a4cccda1e0b5a
MaZderMind <git@mazdermind.de>
2016-02-25 20:53:14 +0000

    co-routine documentation

--- module_hello.lua
+++ module_hello.lua
@@ -8,6 +8,11 @@ function M.can_schedule()
     return true
 end
 
+-- The prepare function is called with the options given in the
+-- playlist json file. This function is called once each time
+-- the module is about to be scheduled. 
+-- The function should return a duration (in seconds) as well
+-- as a value that is later available in the run function.
 function M.prepare(playlist_options)
     duration = 5
     run_options = {
@@ -16,10 +21,22 @@ function M.prepare(playlist_options)
     return duration, run_options
 end
 
+-- This function is running in a coroutine. It gets started
+-- a few seconds before the module should be visible.
+-- duration and options are the values returned by prepare.
+-- fn is a table containing useful functions that can be
+-- used to control the coroutine.
 function M.run(duration, options, fn)
+    -- prepare stuff here
+
+    -- suspend co-routine until the actual display should be started
     fn.wait_t(0)
 
+
+    -- loop in co-routine until the duration is over
     for now in fn.upto_t(duration) do
+        -- co-routine is resumed (and loop-body is executed)
+        -- once for every frame
         font:write(120, 320, "Hello " .. options.text, 100, 1,1,1,1)
     end
 
