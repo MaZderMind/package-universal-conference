b47afd68f7be5652ddd55a0c534bac2ab40ac763
MaZderMind <git@mazdermind.de>
2016-03-01 20:12:17 +0100

    implement configurable scroller size

--- osd/scroller/module.lua
+++ osd/scroller/module.lua
@@ -13,21 +13,24 @@ end
 local text
 util.file_watch("config.json", function(raw)
 	local font
+	local size
 	local speed
 	local color
 
 	if
 		font ~= CONFIG.scroller_font or
+		size ~= CONFIG.scroller_size or
 		speed ~= CONFIG.scroller_speed or
 		color ~= CONFIG.scroller_color.rgba_table
 	then
 		font = CONFIG.scroller_font
+		size = CONFIG.scroller_size
 		speed = CONFIG.scroller_speed
 		color = CONFIG.scroller_color.rgba_table
 
 		text = util.running_text{
 			font = font;
-			size = 40;
+			size = size;
 			speed = speed;
 			color = color;
 			generator = util.generator(feeder)
@@ -45,12 +48,13 @@ function M.hide(duration)
 end
 
 local function draw()
-	local color = CONFIG.scroller_background.rgba_table
+	local bgcolor = CONFIG.scroller_background.rgba_table
+	local size = CONFIG.scroller_size
 
-	local bg = resource.create_colored_texture(unpack(color))
+	local bg = resource.create_colored_texture(unpack(bgcolor))
 	if visibility > 0.01 then
-		bg:draw(0, HEIGHT-45, WIDTH, HEIGHT, 1)
-		text:draw(HEIGHT - visibility * 42)
+		bg:draw(0, HEIGHT-(size + 5), WIDTH, HEIGHT, 1)
+		text:draw(HEIGHT - visibility * (size + 2.5))
 	end
 end
 
