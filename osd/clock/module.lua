b31c80d7763d973be90e547a747df508c7c9fdec
MaZderMind <git@mazdermind.de>
2016-03-03 21:29:29 +0100

    fix padding

--- osd/clock/module.lua
+++ osd/clock/module.lua
@@ -11,33 +11,54 @@ function M.render()
 	local sz = CONFIG.clock_size
 	local w = font:width(txt, sz)
 
+	local padh, padv = 10, 2.5
+
 	local places = {
-		['tl'] = {x = 0; y = 0};
-		['tr'] = {x = WIDTH-w; y = 0};
-		['br'] = {x = WIDTH-w; y = HEIGHT-sz-scroller};
-		['bl'] = {x = 0; y = HEIGHT-sz-scroller};
-		['tc'] = {x = (WIDTH-w)/2; y = 0};
-		['bc'] = {x = (WIDTH-w)/2; y = HEIGHT-sz-scroller};
+		['tl'] = {
+			x = padh;
+			y = padv;
+		};
+		['tr'] = {
+			x = WIDTH - w - padh;
+			y = padv;
+		};
+		['tc'] = {
+			x = (WIDTH - w) / 2;
+			y = padv
+		};
+		['br'] = {
+			x = WIDTH - w - padh;
+			y = HEIGHT - sz - scroller - padv;
+		};
+		['bl'] = {
+			x = padh;
+			y = HEIGHT - sz - scroller - padv;
+		};
+		['bc'] = {
+			x = (WIDTH - w) / 2;
+			y = HEIGHT - sz - scroller - padv;
+		};
 	}
 	local place = places[CONFIG.clock_placement]
 	if place then
 		local bgcolor = CONFIG.clock_background
 		if bgcolor then
 			local bg = resource.create_colored_texture(unpack(bgcolor.rgba_table))
+			-- additional padding is to cater for font specialities
 			bg:draw(
-				place.x,
-				place.y,
-				place.x + w,
-				place.y + sz,
+				place.x - padh - 2.5,
+				place.y - padv - 2.5,
+				place.x + w + padh + 2.5,
+				place.y + sz + padv,
 				1
 			)
 		end
 		if bgimg then
 			bgimg.draw(
-				place.x,
-				place.y,
-				place.x + w,
-				place.y + sz,
+				place.x - padh - 2.5,
+				place.y - padv - 2.5,
+				place.x + w + padh + 2.5,
+				place.y + sz + padv,
 				1
 			)
 		end
