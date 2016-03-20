ad1d1517bd971cb84c16a14fa7c2d3f90452dc55
MaZderMind <git@mazdermind.de>
2016-03-20 20:43:51 +0100

    implement sidebar and some kind of animation

--- osd/sidebar/module.lua
+++ osd/sidebar/module.lua
@@ -2,13 +2,14 @@ local M = {}
 
 local tools             = require "lib/tools"
 local time              = require "lib/time"
+local config            = require "lib/config"
 
 local showhide_speed = 0.05
 local visibility = 0
 local target = 0
 local restore = sys.now() + 1
-
-local hide_angle = 120
+local hide_angle = 110 -- degrees
+local frame = 0
 
 function M.is_enabled()
 	return CONFIG.sidebar_width ~= nil and CONFIG.sidebar_width > 0
@@ -37,14 +38,117 @@ util.data_mapper{
 	end;
 }
 
+local image, next_image, last_image
+local image_transition_duration = 2*60 --seconds
+local image_in_transition = false
+local image_transition_counter = 0
+local valid_until = 0
+
+local function images_init()
+	if not CONFIG.sidebar_images then return end
+
+	print("sidebar_images changed, loading all images")
+	for idx, image in pairs(CONFIG.sidebar_images) do
+		if image.file.type ~= "video" then
+			image.file.load()
+		end
+	end
+end
+config.on_option_changed(
+	{'sidebar_images'},
+	images_init
+)
+images_init()
+
+
+local function images_feeder()
+	return CONFIG.sidebar_images
+end
+local generator = util.generator(images_feeder)
+
 local function draw_images(usable_area)
-	local bg = resource.create_colored_texture(1,0,1,0.75)
-	bg:draw(
-		usable_area.x,
-		usable_area.y,
-		usable_area.x + usable_area.w,
-		usable_area.y + usable_area.h
-	)
+	local now = sys.now()
+
+	if next_image == nil and now > valid_until then
+		next_image = generator.next()
+		valid_until = sys.now() + next_image.duration
+
+		print("image display time is over, selecting next image", next_image.file.asset_name) 
+		if next_image.type == "video" then
+			print("re-loading video", next_image.file.asset_name)
+			next_image.file.unload()
+			next_image.file.load()
+		end
+	end
+
+	if not image_in_transition and next_image and next_image.file:get_surface():state() == 'loaded' then
+		if image == nil then
+			print("next image is loaded and no current image -> jumping transition", next_image.file.asset_name)
+			image = next_image
+			next_image = nil
+		else
+			print("next image is loaded, starting transition", next_image.file.asset_name)
+
+			image_in_transition = true
+			image_transition_counter = image_transition_duration
+		end
+	end
+
+	gl.pushMatrix()
+
+	local width = 0
+	if image ~= nil then
+		local ox1, oy1, ox2, oy2 = util.scale_into(
+			usable_area.w, usable_area.h, image.file.get_surface():size())
+
+		width = ox2 - ox1
+	end
+
+	if image_in_transition then
+		gl.translate(
+			width/2,
+			0
+		)
+		gl.rotate(image_transition_counter / image_transition_duration * 90 - 90, 0, 1, 0)
+		gl.translate(
+			-width/2,
+			0
+		)
+
+		if image_transition_counter > 0 then
+			print("running out-transition", image_transition_counter)
+			image_transition_counter = image_transition_counter - 1
+		elseif image_transition_counter < 0 then
+			print("running in-transition", image_transition_counter)
+			image_transition_counter = image_transition_counter + 1
+		end
+
+		if image_transition_counter == 0 then
+			if next_image ~= nil then
+				print("out-transition finished, swapping images")
+				image = next_image
+				next_image = nil
+
+				image_transition_counter = -image_transition_duration
+			else
+				print("in-transition finished, ending transition cycle. image valid for", image.duration)
+				image_in_transition = false
+			end
+		end
+	end
+
+	if image ~= nil then
+		util.draw_correct(
+			image.file.get_surface(), 
+			usable_area.x,
+			usable_area.y,
+			usable_area.x + usable_area.w,
+			usable_area.y + usable_area.h,
+			1
+		)
+	end
+
+	gl.popMatrix()
 end
 
 local function draw_clock(usable_area)
@@ -78,7 +182,7 @@ local function draw(usable_area)
 	gl.rotate(hide_angle * (1-visibility), 0, 1, 0)
 
 	gl.translate(
-		- usable_area.w,
+		0 - usable_area.w,
 		usable_area.y
 	)
 
@@ -128,6 +232,8 @@ local function draw(usable_area)
 	draw_images(usable_area_images)
 
 	gl.popMatrix()
+
+	frame = frame+1
 end
 
 
